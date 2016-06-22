de5cda5d4dfd969be2340d8d4b2c03921af69829
Luis Metzger <luismetzger@me.com>
2016-06-21 21:52:18 -0600

    Part 3 of homework complete. A little bit easier to understand.

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function daysLeft() {
 	var a = $( "#datepicker" ).datepicker('getDate').getTime();
 	// solutions for 9, 10, 11 and 12 go here:
 
-	// var b = _____
-	// var c = _____
-	// var d = _____
-
+	var b = today.getTime();
+    var c = 24 * 60 * 60 * 1000;
+    var d = Math.floor(Math.round(a - b)/c);
+    console.log(d);
 	// we will remove this later:
 	return a;
 }
@@ -36,7 +36,7 @@ $("#datepicker").on("change", function(a){
 
 // 11. SPOILER ALERT!  If you've done it correctly, you will get a difference in milliseconds, which is not what we want.
 // ...so we need one more variable to store a constant and we need to define it before we store the difference in days.
-// That makes difference in days our FOURTH variable, so let's call our THIRD variable c. 
+// That makes difference in days our FOURTH variable, so let's call our THIRD variable c.
 // c = hours per day * minutes per hour * seconds per minute * ms per sec
 // Do the math and store the value.
 
@@ -46,5 +46,3 @@ $("#datepicker").on("change", function(a){
 // Here is a link to the Math object:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 // I bet you can figure out which method to call on your difference in days variable.
 // It's one of the ones listed under Methods in the left column of the page linked above.
-
-
